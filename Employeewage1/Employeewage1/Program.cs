205f710c43573d6bfa60f5146237e36f3b2164ea
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 23:20:01 +0530

    Initial commit

--- Employeewage1/Employeewage1/Program.cs
+++ Employeewage1/Employeewage1/Program.cs
@@ -6,6 +6,13 @@ namespace Employeewage1
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Start();
+            
+        }
+
+        public void Start()
+        {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
         }
     }
